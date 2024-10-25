1e7f17f211050b9ea5365d7f2b4551c580ee7df1
Anthony Rodriguez <anthony@nezia.dev>
2024-10-26 00:43:35 +0200

    add packages.default

--- flake.nix
+++ flake.nix
@@ -1,17 +1,65 @@
 {
   description = "nezia's portfolio website";
-
-  outputs = {
-    nixpkgs,
-    systems,
-    ...
-  }: let
-    eachSystem = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+  outputs = {nixpkgs, ...}: let
+    eachSystem = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed f;
   in {
-    devShells = eachSystem (pkgs: {
+    devShells = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
       default = pkgs.mkShell {
-        packages = [pkgs.go pkgs.hugo];
+        packages = [
+          pkgs.go_1_22
+          pkgs.hugo
+          pkgs.nodejs
+        ];
       };
+    });
+    packages = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.stdenv.mkDerivation (finalAttrs: {
+        pname = "portfolio";
+        name = finalAttrs.pname;
+        src = ./.;
+        nativeBuildInputs = [
+          pkgs.go
+          pkgs.hugo
+          pkgs.nodejs
+        ];
+        buildPhase = let
+          hugoVendor = pkgs.stdenv.mkDerivation {
+            name = "${finalAttrs.pname}-hugoVendor";
+            inherit (finalAttrs) src nativeBuildInputs;
+            buildPhase = ''
+              hugo mod vendor
+            '';
+            installPhase = ''
+              cp -r _vendor $out
+            '';
+            outputHashMode = "recursive";
+            outputHashAlgo = "sha256";
+            outputHash = "sha256-HTIbu1M3w8escODTPy+c0uNEZNvBbiXAraCsqtyvBiA=";
+          };
+
+          npmDeps = pkgs.buildNpmPackage {
+            name = "${finalAttrs.pname}-npmDeps";
+            inherit (finalAttrs) src nativeBuildInputs;
+
+            npmDepsHash = "sha256-qm12Sv7y0jggO7ygHqEmqLzBzmM1UIlDgo/y31hjBvA=";
+
+            installPhase = ''
+              cp -r node_modules $out/
+            '';
+            dontBuild = true;
+          };
+        in ''
+          cp -r ${hugoVendor} _vendor
+          cp -r ${npmDeps} node_modules
+          hugo --minify -d $out
+        '';
+        dontInstall = true;
+        dontFixup = true;
+      });
     });
   };
   inputs = {
