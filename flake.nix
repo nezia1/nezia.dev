91160f07ebcc1250490f62275fc9b23f514dc062
Anthony Rodriguez <anthony@nezia.dev>
2025-09-16 13:24:57 +0200

    flake: add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,25 @@
 {
   description = "nezia's portfolio website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    tabi = {
+      url = "github:welpo/tabi";
+      flake = false;
+    };
+    resume = {
+      url = "github:nezia1/resume";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
   outputs = {
+    self,
     nixpkgs,
     tabi,
     resume,
-    ...
+    treefmt-nix,
   }: let
     eachSystem = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed f;
     themeName = (builtins.fromTOML (builtins.readFile "${tabi}/theme.toml")).name;
@@ -37,16 +52,11 @@
         installPhase = "cp -r public $out";
       };
     });
-  };
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    tabi = {
-      url = "github:welpo/tabi";
-      flake = false;
-    };
-    resume = {
-      url = "github:nezia1/resume";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
+    formatter = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      treefmtEval = eachSystem (_: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+    in
+      treefmtEval.${system}.config.build.wrapper);
   };
 }
---
+++ treefmt.nix
@@ -0,0 +1,5 @@
+{
+  projectRootFile = "flake.nix";
+  programs.deno.enable = true;
+  programs.alejandra.enable = true;
+}
